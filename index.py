b5837da7ea9ab788bd652d6a987ca905a152d623
Haakasaurus <kramerhjohnson@gmail.com>
2020-04-21 19:45:22 -0700

    Set up test request to stock API

--- index.py
+++ index.py
@@ -1,1 +1,17 @@
-import 'requests'
+import requests
+from config import API_KEY
+
+url = 'https://www.alphavantage.co/query'
+
+response = requests.get(
+    url,
+    params={
+        'function': 'GLOBAL_QUOTE',
+        'symbol': 'ALK',
+        'apikey': API_KEY
+    }
+)
+
+jsonResponse = response.json()
+
+print(jsonResponse['Global Quote']['05. price'])
