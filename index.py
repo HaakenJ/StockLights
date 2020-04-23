1d679dedfb34cc2666a56f1b3252ec9c64e822ca
Haakasaurus <kramerhjohnson@gmail.com>
2020-04-23 15:45:06 -0700

    Added list of owned stocks

--- index.py
+++ index.py
@@ -6,6 +6,7 @@ LIGHT_URL = 'https://api.lifx.com/v1/lights/all/state'
 LIGHT_HEADERS = {
     'Authorization': f'Bearer {LIGHT_TOKEN}'
 }
+symbols = ['ALK', 'MSFT', 'CCL', 'GME', 'NCLH']
 
 stockResponse = requests.get(
     STOCK_URL,
@@ -17,31 +18,34 @@ stockResponse = requests.get(
 )
 
 jsonResponse = stockResponse.json()
+
+print(jsonResponse)
+
 price = jsonResponse['Global Quote']['05. price']
 purchasePrice = 26.46
 
-if float(price) > 26.46:
-    lightResponse = requests.put(
-        LIGHT_URL,
-        data={
-            'power': 'on',
-            'color': 'green',
-            'brightness': 0.1
-        },
-        headers=LIGHT_HEADERS
-    )
+# if float(price) > 26.46:
+#     lightResponse = requests.put(
+#         LIGHT_URL,
+#         data={
+#             'power': 'on',
+#             'color': 'green',
+#             'brightness': 0.1
+#         },
+#         headers=LIGHT_HEADERS
+#     )
 
-    print(lightResponse.json())
-else:
-    lightResponse = requests.put(
-        LIGHT_URL,
-        data={
-            'power': 'on',
-            'color': 'red',
-            'brightness': 0.1
-        },
-        headers=LIGHT_HEADERS
-    )
+#     print(lightResponse.json())
+# else:
+#     lightResponse = requests.put(
+#         LIGHT_URL,
+#         data={
+#             'power': 'on',
+#             'color': 'red',
+#             'brightness': 0.1
+#         },
+#         headers=LIGHT_HEADERS
+#     )
 
-    print(lightResponse.status_code)
+#     print(lightResponse.status_code)
 
