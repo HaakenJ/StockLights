e043267042da14af6e5c038de0f8450c3707de9f
Haakasaurus <kramerhjohnson@gmail.com>
2020-04-24 14:09:21 -0700

    Added folder structure to make package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='stocklights',
+      version='0.1',
+      description='App to watch your stock portfolio and change a light color depending on gains or losses',
+      url='http://github.com/HaakenJ/StockLights',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['funniest'],
+      zip_safe=False)
