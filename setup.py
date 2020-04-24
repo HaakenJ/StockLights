cf0a27ad6d31efc328c8e0105015870125d8ce87
Haakasaurus <kramerhjohnson@gmail.com>
2020-04-24 14:14:13 -0700

    Added dependencies to setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,12 @@ setup(name='stocklights',
       version='0.1',
       description='App to watch your stock portfolio and change a light color depending on gains or losses',
       url='http://github.com/HaakenJ/StockLights',
-      author='Flying Circus',
-      author_email='flyingcircus@example.com',
+      author='HaakenJ',
+      author_email='haaken1234@gmail.com',
       license='MIT',
       packages=['funniest'],
+      install_requires=[
+            'requests',
+            'mysql-connector-python'
+      ],
       zip_safe=False)
