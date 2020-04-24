5defa95f257e68da50314d57503bf3a40156e12a
Haakasaurus <kramerhjohnson@gmail.com>
2020-04-24 12:36:16 -0700

    Added function to get most recent record

--- controller.py
+++ controller.py
@@ -8,22 +8,42 @@ import datetime
     # symbol: string
     # price: double
 
+# Connect to DB
 db = mysql.connector.connect(
     host='localhost',
     user='root',
     password='kramer',
     database='stock_db',
 )
-
+# Create cursor object
 mycursor = db.cursor()
 
+
+
 def create(time, symbol, price):
+    '''
+        Creates a new record in the stocks table.
+
+        @param {Datetime} time
+        @param {String} symbol
+        @param {Double} price
+        @return {None}
+    '''
     mycursor.execute(
         f'INSERT INTO stock_data (entryDate, symbol, price) VALUES ("{time}", "{symbol}", {price});'
     )
     db.commit()
     print(mycursor.rowcount, 'record inserted.')
 
-currentTime = datetime.datetime.now().strftime('%y-%m-%d %H-%M-%S')
 
-create(currentTime, 'ALK', 26.94)
+def getMostRecent():
+    '''
+        Retrieves the most recent record in the stocks table.
+
+        @return {results[]}
+    '''
+    mycursor.execute(
+        'SELECT entryDate, symbol, price FROM stock_data ORDER BY id DESC LIMIT 1'
+    )
+    results = mycursor.fetchall()
+    return results
