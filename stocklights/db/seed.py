e338a35721bcfedf03464a4eaf157af5b82069e3
Haakasaurus <kramerhjohnson@gmail.com>
2020-05-07 12:16:37 -0700

    Removed GME added MARK

--- stocklights/db/seed.py
+++ stocklights/db/seed.py
@@ -18,8 +18,8 @@ parameters = [
     ('ALK', 26.46, 14),
     ('CCL', 11.90, 10),
     ('MSFT', 159.00, 2),
-    ('GME', 4.66, 1),
-    ('NCLH', 11.40, 1)
+    ('NCLH', 11.40, 1),
+    ('MARK', 1.04, 300)
 ]
 query = 'INSERT INTO portfolio (symbol, cost, shares) VALUES (%s, %s, %s)'
 
